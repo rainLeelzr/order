19a4dc1aa44168b7b308cae89ce902dfdfc95043
rainLeelzr <529453975a@sina.com>
2017-09-18 10:13:57 +0800

    init order service

---
+++ src/main/java/avatar/rain/order/controller/TestController.java
@@ -0,0 +1,22 @@
+package avatar.rain.order.controller;
+
+import avatar.rain.order.service.TestService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @Resource
+    private TestService testService;
+
+    @RequestMapping("hello")
+    public String hello() {
+        String s = testService.sayHello();
+        System.out.println(s);
+        return s;
+    }
+}
